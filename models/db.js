25302d7cd4dfa59a1b29710516dfa44d2110740e
Sudhanshu kumar <sudhanshukr8971@gmail.com>
2024-12-30 15:53:40 +0530

    initaial updare on schema

--- models/db.js
+++ models/db.js
@@ -1,19 +1,36 @@
 const mongoose = require("mongoose")
 
+
+const objectid= mongoose.Schema.Types.ObjectId
+
 const userschema= mongoose.Schema({
+    email:String,
+    password:String,
+    first_name: String,
+    last_name:String
 
 })
 
 const courseschema= mongoose.Schema({
+    title:String,
+    description:String,
+    price:Number,
+    imageUrl:String,
+    creatorId:objectid
     
 })
 
 const adminschema= mongoose.Schema({
+    email:String,
+    password:String,
+    first_name: String,
+    last_name:String
     
 })
 
 const purchaseschema= mongoose.Schema({
-    
+    userid:objectid,
+    courseid:objectid
 })
 
 const user = mongoose.model("user",userschema)
